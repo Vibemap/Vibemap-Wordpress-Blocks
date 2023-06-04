cb5e3512aa9340da2ff5c1218a99f11721ec4329
Steve Pepple <stephen.pepple@gmail.com>
2023-06-04 11:03:31 -0700

    Default attributes; sync with state hook

--- src/components/Filters/useFilterState.js
+++ src/components/Filters/useFilterState.js
@@ -7,20 +7,27 @@ import { getVibes, getCategoriesByLevel } from 'vibemap-constants/dist/vibes.js'
 
 const categories1 = getCategoriesByLevel(1)
 const categories2 = getCategoriesByLevel(2)
-const categories = categories1.concat(categories2)
+const categories_all = categories1.concat(categories2)
 const category_slugs = allActivities.activityCategories.map(cat => cat.slug)
 const city_slugs = cities.map(city => city.slug)
 const vibes_slugs = getVibes()
 
-const useFilterState = () => {
-    
-    const [selectedCities, setSelectedCities] = useState([]);
-    const [selectedCategories, setSelectedCategories] = useState([]);
-    const [selectedVibes, setSelectedVibes] = useState([]);
+// Initial state is set in the block.json file
+// Or the current state of the block attributes
+const useFilterState = ({ 
+    cities = [], 
+    categories = [], 
+    vibes = [], 
+    ...props 
+}) => {
+    console.log('DEBUG useFilterState: ', cities, categories, vibes);
+    const [selectedCities, setSelectedCities] = useState(cities);
+    const [selectedCategories, setSelectedCategories] = useState(categories);
+    const [selectedVibes, setSelectedVibes] = useState(vibes);
 
     return { 
         // Data
-        categories,
+        categories_all,
         category_slugs,
         city_slugs,
         vibes_slugs,
