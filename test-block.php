d1476e8e84d87dd35ae70aed5053397ba43be2b3
Steve Pepple <stephen.pepple@gmail.com>
2023-06-06 14:34:47 -0700

    Add the events block; whitespace

--- test-block.php
+++ test-block.php
@@ -31,5 +31,6 @@ require plugin_dir_path( __FILE__ ) . 'build/blocks/01-basic-esnext/index.php';
 require plugin_dir_path( __FILE__ ) . 'build/blocks/02-single-post/index.php';
 require plugin_dir_path( __FILE__ ) . 'build/blocks/03-vibemap-embed/index.php';
 require plugin_dir_path( __FILE__ ) . 'build/blocks/04-vibemap-map-mini/index.php';
+require plugin_dir_path( __FILE__ ) . 'build/blocks/05-vibemap-embed-events/index.php';
 
 add_action( 'init', 'create_block_test_block_block_init' );
