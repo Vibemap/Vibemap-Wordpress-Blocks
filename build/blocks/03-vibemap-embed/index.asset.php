5a742d6b141aa9cf086b670170e87cc6f3bbefb9
Steve Pepple <stephen.pepple@gmail.com>
2023-06-06 17:38:58 -0700

    Rebuild

--- build/blocks/03-vibemap-embed/index.asset.php
+++ build/blocks/03-vibemap-embed/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '7f428fd2fb82a8d2a49c');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '7bc99e9324eda2fff9d6');
